7f04bd5623547d3cd178d911ad7987aed91011dc
Tapasit Suesasiton <killerzeus@hotmail.com>
2019-06-17 18:43:57 +0700

    App2

--- WindowsApp1/frmLogin.vb
+++ WindowsApp1/frmLogin.vb
@@ -1,7 +1,7 @@
 ﻿Public Class frmLogin
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("Button Login Click")
-        MessageBox.Show("Checkbox1");
+        MessageBox.Show("Checkbox1")
         MessageBox.Show("this is a text fromm WinDow App1")
     End Sub
 End Class
---
+++ WindowsApp1/frmUserAdmin.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmUserAdmin
+
+End Class
