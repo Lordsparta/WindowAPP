40de81ac86cc528a21c3dd3dca203f3685f0f3a2
Tapasit Suesasiton <killerzeus@hotmail.com>
2019-06-17 17:57:30 +0700

    Add windowapp2

--- WindowsApp1/frmLogin.vb
+++ WindowsApp1/frmLogin.vb
@@ -1,3 +1,6 @@
 ﻿Public Class frmLogin
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Button Login Click")
+        MessageBox.Show("Checkbox1");
+    End Sub
 End Class
