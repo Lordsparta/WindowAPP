6b495400433a4b3ba0e876c77f72027ef3642491
Tapasit Suesasiton <killerzeus@hotmail.com>
2019-06-17 18:00:42 +0700

    Winapp1

--- WindowsApp1/frmLogin.vb
+++ WindowsApp1/frmLogin.vb
@@ -1,3 +1,5 @@
 ﻿Public Class frmLogin
-
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("this is a text fromm WinDow App1")
+    End Sub
 End Class
