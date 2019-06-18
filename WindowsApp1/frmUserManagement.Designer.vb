dfec627c71b2d46f4f99dd917550ded45fcadd0b
Tapasit Suesasiton <killerzeus@hotmail.com>
2019-06-18 11:26:14 +0700

    14514

--- WindowsApp1/frmUserManagement.Designer.vb
+++ WindowsApp1/frmUserManagement.Designer.vb
@@ -38,6 +38,28 @@ Partial Class frmUserManagement
         Me.LabelX14 = New DevComponents.DotNetBar.LabelX()
         Me.LabelX15 = New DevComponents.DotNetBar.LabelX()
         Me.LabelX16 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX17 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX18 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX19 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX20 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX21 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX22 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX23 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX24 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX25 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX26 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX27 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX28 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX29 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX30 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX31 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX32 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX33 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX34 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX35 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX36 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX37 = New DevComponents.DotNetBar.LabelX()
+        Me.LabelX38 = New DevComponents.DotNetBar.LabelX()
         Me.SuspendLayout()
         '
         'LabelX1
@@ -232,11 +254,297 @@ Partial Class frmUserManagement
         Me.LabelX16.TabIndex = 8
         Me.LabelX16.Text = "LabelX16"
         '
+        'LabelX17
+        '
+        '
+        '
+        '
+        Me.LabelX17.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX17.Location = New System.Drawing.Point(418, 280)
+        Me.LabelX17.Name = "LabelX17"
+        Me.LabelX17.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX17.TabIndex = 31
+        Me.LabelX17.Text = "LabelX17"
+        '
+        'LabelX18
+        '
+        '
+        '
+        '
+        Me.LabelX18.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX18.Location = New System.Drawing.Point(493, 280)
+        Me.LabelX18.Name = "LabelX18"
+        Me.LabelX18.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX18.TabIndex = 30
+        Me.LabelX18.Text = "LabelX18"
+        '
+        'LabelX19
+        '
+        '
+        '
+        '
+        Me.LabelX19.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX19.Location = New System.Drawing.Point(568, 280)
+        Me.LabelX19.Name = "LabelX19"
+        Me.LabelX19.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX19.TabIndex = 29
+        Me.LabelX19.Text = "LabelX19"
+        '
+        'LabelX20
+        '
+        '
+        '
+        '
+        Me.LabelX20.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX20.Location = New System.Drawing.Point(643, 280)
+        Me.LabelX20.Name = "LabelX20"
+        Me.LabelX20.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX20.TabIndex = 28
+        Me.LabelX20.Text = "LabelX20"
+        '
+        'LabelX21
+        '
+        '
+        '
+        '
+        Me.LabelX21.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX21.Location = New System.Drawing.Point(118, 280)
+        Me.LabelX21.Name = "LabelX21"
+        Me.LabelX21.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX21.TabIndex = 27
+        Me.LabelX21.Text = "LabelX21"
+        '
+        'LabelX22
+        '
+        '
+        '
+        '
+        Me.LabelX22.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX22.Location = New System.Drawing.Point(193, 280)
+        Me.LabelX22.Name = "LabelX22"
+        Me.LabelX22.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX22.TabIndex = 26
+        Me.LabelX22.Text = "LabelX22"
+        '
+        'LabelX23
+        '
+        '
+        '
+        '
+        Me.LabelX23.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX23.Location = New System.Drawing.Point(268, 280)
+        Me.LabelX23.Name = "LabelX23"
+        Me.LabelX23.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX23.TabIndex = 25
+        Me.LabelX23.Text = "LabelX23"
+        '
+        'LabelX24
+        '
+        '
+        '
+        '
+        Me.LabelX24.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX24.Location = New System.Drawing.Point(343, 280)
+        Me.LabelX24.Name = "LabelX24"
+        Me.LabelX24.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX24.TabIndex = 24
+        Me.LabelX24.Text = "LabelX24"
+        '
+        'LabelX25
+        '
+        '
+        '
+        '
+        Me.LabelX25.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX25.Location = New System.Drawing.Point(383, 148)
+        Me.LabelX25.Name = "LabelX25"
+        Me.LabelX25.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX25.TabIndex = 23
+        Me.LabelX25.Text = "LabelX25"
+        '
+        'LabelX26
+        '
+        '
+        '
+        '
+        Me.LabelX26.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX26.Location = New System.Drawing.Point(458, 148)
+        Me.LabelX26.Name = "LabelX26"
+        Me.LabelX26.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX26.TabIndex = 22
+        Me.LabelX26.Text = "LabelX26"
+        '
+        'LabelX27
+        '
+        '
+        '
+        '
+        Me.LabelX27.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX27.Location = New System.Drawing.Point(533, 148)
+        Me.LabelX27.Name = "LabelX27"
+        Me.LabelX27.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX27.TabIndex = 21
+        Me.LabelX27.Text = "LabelX27"
+        '
+        'LabelX28
+        '
+        '
+        '
+        '
+        Me.LabelX28.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX28.Location = New System.Drawing.Point(608, 148)
+        Me.LabelX28.Name = "LabelX28"
+        Me.LabelX28.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX28.TabIndex = 20
+        Me.LabelX28.Text = "LabelX28"
+        '
+        'LabelX29
+        '
+        '
+        '
+        '
+        Me.LabelX29.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX29.Location = New System.Drawing.Point(83, 148)
+        Me.LabelX29.Name = "LabelX29"
+        Me.LabelX29.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX29.TabIndex = 19
+        Me.LabelX29.Text = "LabelX29"
+        '
+        'LabelX30
+        '
+        '
+        '
+        '
+        Me.LabelX30.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX30.Location = New System.Drawing.Point(158, 148)
+        Me.LabelX30.Name = "LabelX30"
+        Me.LabelX30.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX30.TabIndex = 18
+        Me.LabelX30.Text = "LabelX30"
+        '
+        'LabelX31
+        '
+        '
+        '
+        '
+        Me.LabelX31.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX31.Location = New System.Drawing.Point(233, 148)
+        Me.LabelX31.Name = "LabelX31"
+        Me.LabelX31.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX31.TabIndex = 17
+        Me.LabelX31.Text = "LabelX31"
+        '
+        'LabelX32
+        '
+        '
+        '
+        '
+        Me.LabelX32.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX32.Location = New System.Drawing.Point(308, 148)
+        Me.LabelX32.Name = "LabelX32"
+        Me.LabelX32.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX32.TabIndex = 16
+        Me.LabelX32.Text = "LabelX32"
+        '
+        'LabelX33
+        '
+        '
+        '
+        '
+        Me.LabelX33.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX33.Location = New System.Drawing.Point(290, 387)
+        Me.LabelX33.Name = "LabelX33"
+        Me.LabelX33.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX33.TabIndex = 34
+        Me.LabelX33.Text = "LabelX33"
+        '
+        'LabelX34
+        '
+        '
+        '
+        '
+        Me.LabelX34.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX34.Location = New System.Drawing.Point(365, 387)
+        Me.LabelX34.Name = "LabelX34"
+        Me.LabelX34.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX34.TabIndex = 33
+        Me.LabelX34.Text = "LabelX34"
+        '
+        'LabelX35
+        '
+        '
+        '
+        '
+        Me.LabelX35.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX35.Location = New System.Drawing.Point(440, 387)
+        Me.LabelX35.Name = "LabelX35"
+        Me.LabelX35.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX35.TabIndex = 32
+        Me.LabelX35.Text = "LabelX35"
+        '
+        'LabelX36
+        '
+        '
+        '
+        '
+        Me.LabelX36.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX36.Location = New System.Drawing.Point(290, 358)
+        Me.LabelX36.Name = "LabelX36"
+        Me.LabelX36.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX36.TabIndex = 37
+        Me.LabelX36.Text = "LabelX36"
+        '
+        'LabelX37
+        '
+        '
+        '
+        '
+        Me.LabelX37.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX37.Location = New System.Drawing.Point(365, 358)
+        Me.LabelX37.Name = "LabelX37"
+        Me.LabelX37.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX37.TabIndex = 36
+        Me.LabelX37.Text = "LabelX37"
+        '
+        'LabelX38
+        '
+        '
+        '
+        '
+        Me.LabelX38.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.LabelX38.Location = New System.Drawing.Point(440, 358)
+        Me.LabelX38.Name = "LabelX38"
+        Me.LabelX38.Size = New System.Drawing.Size(75, 23)
+        Me.LabelX38.TabIndex = 35
+        Me.LabelX38.Text = "LabelX38"
+        '
         'frmUserManagement
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LabelX36)
+        Me.Controls.Add(Me.LabelX37)
+        Me.Controls.Add(Me.LabelX38)
+        Me.Controls.Add(Me.LabelX33)
+        Me.Controls.Add(Me.LabelX34)
+        Me.Controls.Add(Me.LabelX35)
+        Me.Controls.Add(Me.LabelX17)
+        Me.Controls.Add(Me.LabelX18)
+        Me.Controls.Add(Me.LabelX19)
+        Me.Controls.Add(Me.LabelX20)
+        Me.Controls.Add(Me.LabelX21)
+        Me.Controls.Add(Me.LabelX22)
+        Me.Controls.Add(Me.LabelX23)
+        Me.Controls.Add(Me.LabelX24)
+        Me.Controls.Add(Me.LabelX25)
+        Me.Controls.Add(Me.LabelX26)
+        Me.Controls.Add(Me.LabelX27)
+        Me.Controls.Add(Me.LabelX28)
+        Me.Controls.Add(Me.LabelX29)
+        Me.Controls.Add(Me.LabelX30)
+        Me.Controls.Add(Me.LabelX31)
+        Me.Controls.Add(Me.LabelX32)
         Me.Controls.Add(Me.LabelX9)
         Me.Controls.Add(Me.LabelX10)
         Me.Controls.Add(Me.LabelX11)
@@ -275,4 +583,26 @@ Partial Class frmUserManagement
     Friend WithEvents LabelX14 As DevComponents.DotNetBar.LabelX
     Friend WithEvents LabelX15 As DevComponents.DotNetBar.LabelX
     Friend WithEvents LabelX16 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX17 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX18 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX19 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX20 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX21 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX22 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX23 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX24 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX25 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX26 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX27 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX28 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX29 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX30 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX31 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX32 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX33 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX34 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX35 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX36 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX37 As DevComponents.DotNetBar.LabelX
+    Friend WithEvents LabelX38 As DevComponents.DotNetBar.LabelX
 End Class
